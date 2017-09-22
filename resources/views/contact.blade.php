1fa8381bd994b9174ac09ae41977019a040f82b4
Brandon Plentl <bplentl@gmail.com>
2017-09-22 11:14:27 -0500

    Adding Base Pages

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('page-title')HOME
+@endsection
+
+@section('head')
+    <!-- CODE HERE -->
+@endsection
+
+@section('main-content')
+    <!-- CODE HERE -->
+@endsection
+
+@section('footer-js')
+    <!-- CODE HERE -->
+@endsection
