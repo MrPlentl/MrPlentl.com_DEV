d78fe7f216dcbe908445af126e683753fef45b87
Brandon Plentl <bplentl@gmail.com>
2017-11-05 06:38:23 -0600

    Updated DB

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,13 +4,13 @@
 @endsection
 
 @section('main-content')
-			<section class="no-pad error-page bg-primary fullscreen-element">
+			<section class="no-pad error-page bg-primary fullscreen-element" style="background: url('/img/404-last-of-us.jpg');background-size: cover;">
 				<div class="container align-vertical">
 					<div class="row">
 						<div class="col-sm-12 text-center">
 							<i class="icon icon-compass"></i>
-							<h1 class="jumbo">404</h1>
-							<h1><strong>Oh dear, we seem to have led you astray!</strong><br>Let's get back on track...</h1>
+							<h1 class="jumbo" style="text-shadow: 5px 3px 25px #000000;">404</h1>
+							<h1 style="text-shadow: 5px 3px 25px #000000;"><strong>Oh dear, we seem to have led you astray!</strong><br>Let's get back on track...</h1>
 							<a href="chooser.html" class="btn btn-primary btn-white" target="default">Take Me Home</a>
 							<a href="#" class="btn btn-primary btn-white btn-text-only">Report This</a>
 						</div>
